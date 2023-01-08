82ff80af00e3d93e018156d9fb61414e4625747f
wojtek <wojti39@gmail.com>
2023-01-08 21:46:10 +0100

    initial code

---
+++ tic_tac_toe/library/src/tic_tac_toe/logic/models.py
@@ -0,0 +1,10 @@
+import enum
+
+
+class Mark(str, enum.Enum):
+    CROSS = "X"
+    NAUGHT = "O"
+
+    @property
+    def other(self) -> "Mark":
+        return Mark.CROSS if self is Mark.NAUGHT else Mark.NAUGHT
